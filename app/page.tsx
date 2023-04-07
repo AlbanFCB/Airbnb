b2aaba00a1b3cec56d11a17cde92e0a89d5df4e7
Alban Cavalli <alban.cavalli7@etu.univ-lorraine.fr>
2023-04-07 12:08:17 +0200

    Favorite

--- app/page.tsx
+++ app/page.tsx
@@ -11,36 +11,36 @@ export default async function Home() {
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
-    <ClientOnly>
-      <EmptyState showReset/>
-    </ClientOnly>
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
   }
 
   return (
     <ClientOnly>
       <Container>
-        <div className="
-        pt-24
-        grid 
-        grid-cols-1 
-        sm:grid-cols-2 
-        md:grid-cols-3 
-        lg:grid-cols-4 
-        xl:grid-cols-5 
-        2xl:grid-cols-6
-        gap-8
-        ">
-          <div className="">
-            {listings.map((listing: any) => {
-              return (
-                <ListingCard
-                currentUser={currentUser}
-                key={listing.id}
-                data={listing}
-                />
-              )
-            })}
-          </div>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
